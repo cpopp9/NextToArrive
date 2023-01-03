e06721affa1679de7868c9a2a1af5052d38509d9
cpopp9 <104847711+cpopp9@users.noreply.github.com>
2023-01-03 13:05:19 -0500

    Added date formatter function

--- NextToArrive/ContentView.swift
+++ NextToArrive/ContentView.swift
@@ -1,22 +1,67 @@
-//
-//  ContentView.swift
-//  NextToArrive
-//
-//  Created by Cory Popp on 1/3/23.
-//
+    //
+    //  ContentView.swift
+    //  NextToArrive
+    //
+    //  Created by Cory Popp on 1/3/23.
+    //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var timeRemaining = 5
+    let timer = Timer.publish(every: 60, on: .main, in: .common).autoconnect()
+    
+    let nextScheduled = "01:41p"
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.green.ignoresSafeArea()
+            
+            VStack {
+                Text("\(timeRemaining) minutes")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                Text("Until the next bus")
+                    .foregroundColor(.white)
+                
+                Text("-")
+                    .foregroundColor(.white)
+                
+                Text("Scheduled to arrive at \(nextScheduled)")
+                    .foregroundColor(.white)
+                
+                Button() {
+                    dateFormatter()
+                } label: {
+                    Text("Format Date")
+                        .foregroundColor(.white)
+                }
+            }
+            .onReceive(timer) { time in
+                if timeRemaining > 0 {
+                    timeRemaining -= 1
+                }
+            }
+            
+            .padding()
         }
-        .padding()
     }
+    
+    func dateFormatter() {
+        
+        let dateFormatter = DateFormatter()
+        
+            // Set Date Format
+        dateFormatter.dateFormat = "hh:mm"
+        
+            // Convert String to Date
+        dateFormatter.date(from: nextScheduled)
+    }
+    
+    
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
