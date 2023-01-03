dcc14e28534504fe3a549736e64c7b82c0fb9414
cpopp9 <104847711+cpopp9@users.noreply.github.com>
2023-01-03 12:30:13 -0500

    Initial Commit

---
+++ NextToArrive/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NextToArrive
+//
+//  Created by Cory Popp on 1/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NextToArrive/NextToArriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextToArriveApp.swift
+//  NextToArrive
+//
+//  Created by Cory Popp on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct NextToArriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
