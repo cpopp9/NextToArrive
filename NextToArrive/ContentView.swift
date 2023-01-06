a489b59b31d8d1ffc01a726b178b0c5326c423d0
cpopp9 <104847711+cpopp9@users.noreply.github.com>
2023-01-06 15:24:17 -0500

    - Added stale time removal method - Added time comparison method improvements - Added UI improvements

--- NextToArrive/ContentView.swift
+++ NextToArrive/ContentView.swift
@@ -10,32 +10,86 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var busTime = "01:18p"
+    @State var currentT = Date()
+    
+    @State var busTimes: [String] = ["2:53p", "3:00p", "3:20p", "5:00p"]
+    
+        // Convert both the current time and next upcoming scheduled bus into total number of minutes
+    var currentTime: Int {
+        60*Calendar.current.component(.hour, from: currentT) + Calendar.current.component(.minute, from: currentT)
+    }
+    
+    var nextTime: Int {
+        60*Calendar.current.component(.hour, from: dateFormatter(nextScheduled: busTimes[0])) + Calendar.current.component(.minute, from: dateFormatter(nextScheduled: busTimes[0]))
+    }
+    
+    var TimeDiff: Int {
+        
+            // Calculate the number of minutes between now and when the bus arrives
+        var timeDifference: Int {
+            nextTime - currentTime
+        }
+        
+        return timeDifference
+        
+    }
     
     var body: some View {
-        ZStack {
-            Color.green.ignoresSafeArea()
-            
-            VStack {
+        NavigationView {
+            ZStack {
+                Color.green.ignoresSafeArea()
                 
-                Spacer()
-                
-                HStack {
-                    Text(String(compareTimes(busTime: busTime)))
-                        .font(.system(size: 100).bold())
-                    Text("Minutes")
-
-                        .font(.title3.bold())
+                VStack {
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Text(String(TimeDiff))
+                            .font(.system(size: 100).bold())
+                        Text("Minutes")
+                            .font(.title3.bold())
+                    }
+                    
+                    Text("Until the next bus")
+                    
+                    Spacer()
+                    
+                    Text("Scheduled to arrive at \(busTimes[0])")
                 }
-                Text("Until the next bus")
-                
-                Spacer()
-                
-                Text("Scheduled to arrive at \(busTime)")
+                .padding()
             }
-            .padding()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button() {
+                        removeTimes()
+                    } label: {
+                        Image(systemName: "plus")
+                            .foregroundColor(.white)
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button() {
+                        currentT = Date()
+                    } label: {
+                        Image(systemName: "arrow.clockwise")
+                            .foregroundColor(.white)
+                    }
+                }
+            }
         }
     }
+    
+    
+    func removeTimes() {
+        for time in busTimes {
+            
+            if 60*Calendar.current.component(.hour, from: dateFormatter(nextScheduled: time)) + Calendar.current.component(.minute, from: dateFormatter(nextScheduled: time)) < currentTime {
+                busTimes.remove(at: 0)
+            }
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
--- NextToArrive/Extensions/ContentViewVM.swift
+++ NextToArrive/Extensions/ContentViewVM.swift
@@ -9,25 +9,14 @@ import Foundation
 
 extension ContentView {
     
-    func compareTimes(busTime: String) -> Int {
-        
+    func dateFormatter(nextScheduled: String) -> Date {
             // Create Date Formatter
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "hh:mma"
         
             // Convert String to Date
-        let nextScheduled = dateFormatter.date(from: busTime) ?? Date.now
         
-            // Convert both the current time and next upcoming scheduled bus into total number of minutes
-        let currentTime = 60*Calendar.current.component(.hour, from: Date()) + Calendar.current.component(.minute, from: Date())
-        let nextTime = 60*Calendar.current.component(.hour, from: nextScheduled) + Calendar.current.component(.minute, from: nextScheduled)
-        
-            // Calculate the number of minutes between now and when the bus arrives
-        var timeDifference: Int {
-            nextTime - currentTime
-        }
-        
-        return timeDifference
+        return dateFormatter.date(from: nextScheduled) ?? Date.now
     }
     
 }
